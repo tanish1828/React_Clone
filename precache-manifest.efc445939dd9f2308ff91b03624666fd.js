9507bb973aa08a932234326d27d02db0cfcd289a
tanish1828 <75876896+tanish1828@users.noreply.github.com>
2020-12-12 12:52:41 +0530

    Add files via upload

---
+++ precache-manifest.efc445939dd9f2308ff91b03624666fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dd54f10c0b4f9e3f68d3dcfbd01e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d3b010ea7302324bcd",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d3b010ea7302324bcd",
+    "url": "/static/js/main.47d7e54f.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
